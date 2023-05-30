f461404bff28e3a4f2ea2a0251abdcc2a5337afe
Harold Peña <haroldpc258@gmail.com>
2023-05-30 17:02:25 -0500

    first commit
    Se ha realizado la solución hasta el ejercicio 14 de Aprendizaje

---
+++ JavaIntro/src/ejercicios/ejerciciosAprendizaje/ejercicio4/Ejercicio4.java
@@ -0,0 +1,15 @@
+package ejercicios.ejerciciosAprendizaje.ejercicio4;
+
+import java.util.Scanner;
+
+public class Ejercicio4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        /*int num = 0;
+        do {
+            System.out.print("Indique un número: ");
+            num = scanner.nextInt();
+        } while (num != 2 & num !=4  & num !=6);*/
+    }
+}
